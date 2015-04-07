e247fc29a9789c72c5b54e4b5b36568f22c3d08e
John Sullivan <jsullivan@csumb.edu>
2015-04-06 23:28:13 -0700

    define integration tests

--- src/test/java/com/example/test/AppTest.java
+++ src/test/java/com/example/test/AppTest.java
@@ -1,20 +1,67 @@
 package com.example.test;
-import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
 public class AppTest {
 
     @Test
-    public void thisAlwaysPasses() {
-
+    public void groupsHaveThreePeople() throws Exception {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        App app = new App(names);
+        List<String> group = app.createGroup("a");
+        assertEquals(group.size(), 3);
     }
 
     @Test
-    @Ignore
-    public void thisIsIgnored() {
+    public void givenUsernameIsInGroup() throws Exception {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        App app = new App(names);
+        String name = "a";
+        List<String> group = app.createGroup(name);
+        assertEquals(group.contains(name), true);
     }
+
+    @Test
+    public void groupsHaveConsistentMembership() throws Exception {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        App app = new App(names);
+        List<String> actualA = app.createGroup("a");
+        List<String> actualB = app.createGroup("b");
+        List<String> actualC = app.createGroup("c");
+        List<String> expected = new ArrayList<String>();
+        expected.add("a");
+        expected.add("b");
+        expected.add("c");
+        assertEquals(actualA, expected);
+        assertEquals(actualB, expected);
+        assertEquals(actualC, expected);
+    }
+
 }
